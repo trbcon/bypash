38fde25e6d6c14f4ecf81af4ab3b8ba05fc24b5a
trbcon <pahax.off@gmail.com>
2025-06-04 23:00:18 +0300

    display

--- bypash.ino
+++ bypash.ino
@@ -73,32 +73,8 @@ void handleBack() {
   }
 }
 
-void drawMenu() {
-  tft.fillScreen(TFT_BLACK);
-  tft.setCursor(0, 0);
-  tft.setTextColor(TFT_CYAN, TFT_BLACK);
-  tft.println(currentMenu->name);
 
-  for (int i = 0; i < maxVisibleItems; i++) {
-    int menuIndex = viewOffset + i;
-    if (menuIndex >= currentMenu->items.size()) break;
 
-    int y = (i + 1) * 10;
-
-    if (menuIndex == selectedItem) {
-      tft.setTextColor(TFT_YELLOW, TFT_BLACK);
-      tft.setCursor(0, y);
-      tft.print(">");
-    } else {
-      tft.setCursor(0, y);
-      tft.print(" ");
-    }
-
-    tft.setCursor(10, y);
-    tft.setTextColor(TFT_WHITE, TFT_BLACK);
-    tft.print(currentMenu->items[menuIndex]);
-  }
-}
 
 
 void executeAction(String label) {
@@ -117,3 +93,7 @@ void executeAction(String label) {
 }
 
 
+// 16.10
+// 47.00
+// 56.20
+// 58.00
--- display.h
+++ display.h
@@ -32,3 +32,30 @@ void setupMenus() {
   bluetoothMenu.name = "Bluetooth";
   bluetoothMenu.items = {"Bluetooth connect", "Bluetooth scan", "back"};
 }
+
+void drawMenu() {
+  tft.fillScreen(TFT_BLACK);
+  tft.setCursor(0, 0);
+  tft.setTextColor(TFT_CYAN, TFT_BLACK);
+  tft.println(currentMenu->name);
+
+  for (int i = 0; i < maxVisibleItems; i++) {
+    int menuIndex = viewOffset + i;
+    if (menuIndex >= currentMenu->items.size()) break;
+
+    int y = (i + 1) * 10;
+
+    if (menuIndex == selectedItem) {
+      tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+      tft.setCursor(0, y);
+      tft.print(">");
+    } else {
+      tft.setCursor(0, y);
+      tft.print(" ");
+    }
+
+    tft.setCursor(10, y);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    tft.print(currentMenu->items[menuIndex]);
+  }
+}
